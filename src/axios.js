1dd8152433c06cc3b58b50b66784e44079d08363
Raiko Kalev <rakale@ttu.ee>
2021-03-31 13:24:29 +0300

    learning routing

--- src/axios.js
+++ src/axios.js
@@ -4,6 +4,8 @@ const instance = axios.create({
     baseURL: 'https://jsonplaceholder.typicode.com'
 });
 
-instance.defaults.headers.common["Authorization"] = "AUTH TOKEN FROM INSTANCE";
+instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
+
+// instance.interceptors.request...
 
 export default instance;
--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -2,33 +2,43 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import './NewPost.css';
+import {Redirect} from 'react-router-dom';
 
 class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Max',
+        submitted:false
     }
 
-    componentDidMount(){
+    componentDidMount () {
         console.log(this.props);
     }
 
     postDataHandler = () => {
-        const post = {
+        const data = {
             title: this.state.title,
             body: this.state.content,
             author: this.state.author
         };
-        axios.post("/posts/", post)
-        .then(response=>{
-            console.log(response);
-        });
+        axios.post('/posts', data)
+            .then(response => {
+                console.log(response);
+                this.props.history.replace('/posts');
+                //this.setState({submitted:true})
+            });
     }
 
     render () {
+
+        let redirect=null;
+        if(this.state.submitted){
+            redirect= <Redirect to="/posts"/>
+        }
         return (
             <div className="NewPost">
+                {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
